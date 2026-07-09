d601c2d3ce5ce0df059abd31fb7e0a77c07718b2
yuki <y09425717@gmail.com>
2026-07-09 17:01:33 +0900

    スキーマにidを追加し、課題要件に合わせる

--- 03.asynchronous/callback-no-error.js
+++ 03.asynchronous/callback-no-error.js
@@ -1,12 +1,15 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-db.run("CREATE TABLE books (title TEXT)", () => {
-  db.run("INSERT INTO books VALUES ('タイトル')", function () {
-    console.log(this.lastID);
-    db.get("SELECT rowid AS id, title FROM books", (err, row) => {
-      console.log(row.id, row.title);
-      db.run("DROP TABLE books");
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run("INSERT INTO books(title) VALUES ('タイトル')", function () {
+      console.log(this.lastID);
+      db.get("SELECT id, title FROM books", (err, row) => {
+        console.log(row.id, row.title);
+        db.run("DROP TABLE books");
+      });
     });
-  });
-});
+  },
+);
