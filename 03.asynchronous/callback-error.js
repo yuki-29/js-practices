08193eb45613247757d9a6bda2b5c3d3cead670f
yuki <y09425717@gmail.com>
2026-07-10 15:00:05 +0900

    callbackエラーありのプログラムを実装

---
+++ 03.asynchronous/callback-error.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
+
+const sqlQuery = "INSERT INTO books(title) VALUES ('タイトル')";
+
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run(sqlQuery, () => {
+      db.run(sqlQuery, function (insertError) {
+        if (insertError) {
+          console.error(insertError.message);
+        }
+
+        db.get("SELECT id, author FROM books", (selectError) => {
+          if (selectError) {
+            console.error(selectError.message);
+          }
+          db.run("DROP TABLE books");
+        });
+      });
+    });
+  },
+);
