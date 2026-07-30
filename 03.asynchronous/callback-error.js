9635407136dcd90cfab6fb5348674efda1af0c38
yuki <y09425717@gmail.com>
2026-07-30 13:34:43 +0900

    変数名を名詞形に修正

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -4,13 +4,13 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const insertBook = "INSERT INTO books(title) VALUES ('タイトル')";
+const insertQuery = "INSERT INTO books(title) VALUES ('タイトル')";
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run(insertBook, () => {
-      db.run(insertBook, (insertError) => {
+    db.run(insertQuery, () => {
+      db.run(insertQuery, (insertError) => {
         if (insertError) {
           console.error(insertError.message);
         }
