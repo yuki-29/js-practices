320b26af3a9906f79a3130855d3b05006882b0ab
yuki <y09425717@gmail.com>
2026-07-30 11:44:23 +0900

    thisを使うコールバックへのコメント追加

--- 03.asynchronous/callback-no-error.js
+++ 03.asynchronous/callback-no-error.js
@@ -7,6 +7,7 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
+    // this.lastIDを参照する為、functionを使用。
     db.run("INSERT INTO books(title) VALUES ('タイトル')", function () {
       console.log(this.lastID);
       db.get("SELECT id, title FROM books", (_error, row) => {
