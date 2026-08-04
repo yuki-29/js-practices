297458e2f23da5cf29c972084a3694de7f7d41b2
yuki <y09425717@gmail.com>
2026-08-04 20:07:42 +0900

    Top-level awaitを使用する方法へ修正

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -2,24 +2,30 @@
 
 import { dbRun, dbGet, dbClose } from "./database.js";
 
-async function main() {
-  try {
-    await dbRun(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    await dbRun("INSERT INTO books(title) VALUES ('タイトル')");
-    await dbRun("INSERT INTO books(title) VALUES ('タイトル')");
-  } catch (insertError) {
-    console.error(insertError.message);
-  }
+const insertQuery = "INSERT INTO books(title) VALUES ('タイトル')";
 
-  try {
-    await dbGet("SELECT id, author FROM books");
-  } catch (selectError) {
-    console.error(selectError.message);
+await dbRun(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+await dbRun(insertQuery);
+try {
+  await dbRun(insertQuery);
+} catch (insertError) {
+  if (insertError.code === "SQLITE_CONSTRAINT") {
+    console.error(insertError.message);
+  } else {
+    throw insertError;
   }
-  await dbRun("DROP TABLE books");
-  await dbClose();
 }
 
-main();
+try {
+  await dbGet("SELECT id, author FROM books");
+} catch (selectError) {
+  if (selectError.code === "SQLITE_ERROR") {
+    console.error(selectError.message);
+  } else {
+    throw selectError;
+  }
+}
+await dbRun("DROP TABLE books");
+await dbClose();
--- 03.asynchronous/async-await-no-error.js
+++ 03.asynchronous/async-await-no-error.js
@@ -2,18 +2,14 @@
 
 import { dbRun, dbGet, dbClose } from "./database.js";
 
-async function main() {
-  await dbRun(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  const result = await dbRun("INSERT INTO books(title) VALUES ('タイトル')");
-  console.log(result.lastID);
+await dbRun(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+const result = await dbRun("INSERT INTO books(title) VALUES ('タイトル')");
+console.log(result.lastID);
 
-  const row = await dbGet("SELECT id, title FROM books");
-  console.log(row.id, row.title);
+const row = await dbGet("SELECT id, title FROM books");
+console.log(row.id, row.title);
 
-  await dbRun("DROP TABLE books");
-  await dbClose();
-}
-
-main();
+await dbRun("DROP TABLE books");
+await dbClose();
