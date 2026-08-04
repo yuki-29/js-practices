a4c62a5c743acab28a98fbec8dd2e65637598502
yuki <y09425717@gmail.com>
2026-08-04 15:50:18 +0900

    console.logを後続の非同期処理と同じthenにまとめる

--- 03.asynchronous/promise-no-error.js
+++ 03.asynchronous/promise-no-error.js
@@ -8,14 +8,10 @@ dbRun(
   .then(() => dbRun("INSERT INTO books(title) VALUES ('タイトル')"))
   .then((result) => {
     console.log(result.lastID);
-  })
-  .then(() => {
     return dbGet("SELECT id, title FROM books");
   })
   .then((row) => {
     console.log(row.id, row.title);
-  })
-  .then(() => {
     return dbRun("DROP TABLE books");
   })
   .then(() => dbClose());
