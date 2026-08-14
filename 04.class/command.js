4f2df26fa132cd89a51cef1c8a80074505e3be4a
yuki <y09425717@gmail.com>
2026-08-14 17:20:15 +0900

    UserIntrfaceのインスタンス名を変更

--- 04.class/command.js
+++ 04.class/command.js
@@ -10,32 +10,29 @@ export class Command {
   async run() {
     const db = new Database();
     await db.setup();
-    const read = new UserInterface();
+    const ui = new UserInterface();
 
     try {
       if (!this.argv.l && !this.argv.r && !this.argv.d) {
-        const lines = await read.readLine();
+        const lines = await ui.readLine();
         await db.add(lines.join("\n"));
         return;
       }
 
       const memos = await db.all();
       if (memos.length === 0) {
-        read.notifyEmpty();
+        ui.notifyEmpty();
         return;
       }
 
       if (this.argv.l) {
-        read.showList(memos);
+        ui.showList(memos);
       } else if (this.argv.r) {
-        const id = await read.choices(memos, "Choose a note you want to see:");
+        const id = await ui.choices(memos, "Choose a note you want to see:");
         const targetMemo = memos.find((memo) => memo.id === id);
-        read.showBody(targetMemo);
+        ui.showBody(targetMemo);
       } else if (this.argv.d) {
-        const id = await read.choices(
-          memos,
-          "Choose a memo you want to delete:",
-        );
+        const id = await ui.choices(memos, "Choose a memo you want to delete:");
         await db.destroy(id);
       }
     } finally {
